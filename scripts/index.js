0b1b5ee9afa51586382207ce9ef6fd752c54b300
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2023-04-27 23:42:05 -0400

    feat:Se crea el template para las Cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,46 @@
+/*A */
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  }
+];
+const containerCard = document.querySelector('.card-container');
+
+const initialRender = initialCards.map((initialCard) => {
+  const templateCard = document.querySelector('.card-container').content;
+  const elementCard = templateCard.querySelector('.card').cloneNode(true);
+
+  elementCard.querySelector('.card__name').textContent = initialCard.name;
+  elementCard.querySelector('.card__image').setAttribute('src', `${initialCard.link}`);
+
+
+  containerCard.append(elementCard);
+})
+
+
+
+
 /*Declaración de variables*/
 /* ============================== */
 const modal = document.querySelector('.modal');
@@ -9,7 +52,7 @@ const btn_guardar = document.querySelector('.modal__btn-guardar');
 
 /*Función para Editar y cerrar Modal*/
 /***************************************** */
-function editarPerfil(){
+function editarPerfil() {
   modal.style.display = 'flex';
   const valorNombre = nameInput.innerText;
   const valorAcerca = jobInput.innerText;
@@ -19,7 +62,7 @@ function editarPerfil(){
 
 }
 const btn__editar = document.querySelector('.btn__edit');
-btn__editar.addEventListener('click' , editarPerfil);
+btn__editar.addEventListener('click', editarPerfil);
 /* window.onload = editarPerfil; */
 
 /* ========== */
@@ -50,22 +93,22 @@ const add = document.querySelector('.btn__add');
 const close = document.querySelector('.modal__btn-close');
 /* add.style.opacity = '0.8' */
 
-function addOpacidad (){
+function addOpacidad() {
   add.style.opacity = '0.6';
 }
-function removeAddOpacidad (){
+function removeAddOpacidad() {
   add.style.opacity = '1';
 }
-function editOpacidad(){
+function editOpacidad() {
   edit.style.opacity = '0.6';
 }
-function removeEditOpacidad(){
+function removeEditOpacidad() {
   edit.style.opacity = '1';
 }
-function closeOpacidad (){
+function closeOpacidad() {
   close.style.opacity = '0.6';
 }
-function removeCloseOpacidad (){
+function removeCloseOpacidad() {
   close.style.opacity = '1';
 }
 
