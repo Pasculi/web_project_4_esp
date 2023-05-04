24675362989b6d6124d47be221bad2c587beb92c
Pasculi <jorge.sepulveda.turpie@gmail.com>
2023-05-04 07:32:03 -0400

    feat:Función mostrar imagen v1.2

--- scripts/index.js
+++ scripts/index.js
@@ -116,7 +116,22 @@ function savePlaces(e) {
   const elementCard = templateCard.querySelector(".card").cloneNode(true);
   elementCard.querySelector(".card__name").textContent = `${card.name}`;
   elementCard.querySelector(".card__image").setAttribute("src", `${card.link}`);
-  
+  /**CLICK EN IMAGEN */
+  elementCard.querySelector('.card__image').addEventListener("click", function (evt) {
+    console.log('Click en imagen');
+    const imagenPlace= document.querySelector('.modal__image-place');
+    const namePlace = document.querySelector('.name__place');
+    imagenPlace.setAttribute("src", evt.target.src);
+    namePlace.textContent = card.name;
+    
+    console.log(evt.target);
+    console.log(evt.target.src);
+    console.log(evt.target.alt);
+
+    const modalImagen = document.querySelector('.modal__image');
+    modalImagen.style.display ="flex"
+  });
+  /**CLICK EN IMAGEN */
   elementCard
     .querySelector(".btn__delete")
     .addEventListener("click", function (evt) {
