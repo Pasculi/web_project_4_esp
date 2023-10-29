3b4ee465e4292a7c0f9308cb83bb128d4896a15d
Rodrigo Turpie <jsepulveda_turpie@hotmail.com>
2023-10-29 02:28:07 -0300

    Archivo constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,35 +1,4 @@
-import { config } from "../components/FormValidator.js"
-
-
-const initialCards = [
-  {
-    name: "Santiago",
-    link: "https://storage.googleapis.com/chile-travel-newsite-static-content/2021/08/santiago-portada.jpg",
-  },
-  {
-    name: "Valdivia",
-    link: "https://storage.googleapis.com/chile-travel-newsite-static-content/2021/07/Encantos_Valdivia-y-Corral_mercado-fluvialjpg-1024x683.jpg",
-  },
-  {
-    name: "Puerto de Valparaíso",
-    link: "https://media.istockphoto.com/id/827067390/es/foto/colorido-valparaiso.jpg?s=612x612&w=0&k=20&c=VsJWIW249JHwoSUfpIhCYjl7gELpdCLxa0trOo_nqjk=",
-  },
-  {
-    name: "Calama",
-    link: "https://cdnx.jumpseller.com/ad-toma-aerea-banco-de-im/image/21301255/Captura_de_Pantalla_2021-12-17_a_la_s__19.49.20.jpg?1639781545",
-  },
-  {
-    name: "Antofagasta",
-    link: "https://www.holidayinnexpress.cl/wp-content/uploads/2017/08/destacada_antofagasta.jpg",
-  },
-  {
-    name: "Ciudad de Arica",
-    link: "https://q-xx.bstatic.com/xdata/images/city/600x480/671802.jpg?k=f2af7c265359c6bfef67dd8137aae05a987d3df4208c8423068a807acc405ad3&o=",
-  },
-];
-
-
-
+import { config } from "./FormValidator.js"
 
 /*Función que aplica opacidad a los botones editar, añadir lugar y a las X de cerrar */
 const btns = Array.from(document.querySelectorAll('.btn'));
@@ -47,7 +16,6 @@ const opacityButtons = () => {
 
 }
 
-
 /*Funcion para dar like al corazón*/
 const buttonLike = (evt) => {
   evt.target.classList.toggle("card__place-button--like-active");
@@ -58,22 +26,14 @@ const buttonDeleteCard = (evt) => {
   evt.target.parentNode.parentNode.remove();
 }
 
-
-
 /*Abrir y cerrar Popup_Form*/
 const editProfile = document.querySelector('.profile__author-button-edit');
 const editPlace = document.querySelector('.profile__author-button-add-place');
-const popupImage = document.querySelector('.popup-image')
-
 const popupCloseProfile = document.querySelector('.popup__button-close-profile');
 const popupClosePlace = document.querySelector('.popup__button-close-place');
 const popupCloseImage = document.querySelector('.popup-image__button-close');
 const popupProfile = document.querySelector('.popup-profile');
 const popupPlace = document.querySelector('.popup-place');
-
-
-const inputName = document.querySelector("#popup__input-profile").value
-const inputJob = document.querySelector("#popup__input-about").value
 
 function showPopupProfile() {
   popupProfile.classList.add('popup--show');
@@ -88,8 +48,8 @@ function showPopupPlace() {
 }
 function closePopupPlace() {
   popupPlace.classList.remove('popup--show');
-  inputName = "";
-  inputJob = "";
+  document.querySelector("#popup__input-place").value = "";
+  document.querySelector("#popup__input-url").value = "";
 }
 editProfile.addEventListener('click', showPopupProfile);
 editPlace.addEventListener('click', showPopupPlace);
@@ -104,7 +64,7 @@ document.addEventListener('keydown', evt => {
 
 
 /*Funcion para desplagar una imagen */
-
+const popupImage = document.querySelector('.popup-image')
 function openPopupImage() {
   popupImage.classList.add('popup-image--show')
 }
@@ -119,23 +79,25 @@ document.addEventListener('keydown', evt => {
   }
 });
 
-
-
-/* const popupEditProfile = () => {
+/*Cambiar Nombre de Perfil y Acerca de en Perfil*/
+const buttonProfile = document.querySelector('.popup__button-profile')
+function popupEditProfile() {
+  const nameProfile = document.querySelector('.profile__author');
+  const activitProfile = document.querySelector('.profile__activit');
   const inputsElements = Array.from(document.querySelectorAll(config.inputSelector))
   inputsElements.forEach(inputElement => {
     inputElement.addEventListener('input', evt => {
       if (evt.target.id === 'popup__input-profile') {
-        userName.textContent = evt.target.value.trim();
+        nameProfile.textContent = evt.target.value.trim();
       }
       else if (evt.target.id === 'popup__input-about') {
-        userJob.textContent = evt.target.value.trim();
+        activitProfile.textContent = evt.target.value.trim();
       }
     })
   })
 }
 popupEditProfile()
-buttonProfile.addEventListener('click', closePopupProfile) */
+buttonProfile.addEventListener('click', closePopupProfile)
 
 
-export { initialCards, openPopupImage, closePopupImage, showPopupProfile, closePopupProfile, showPopupPlace, closePopupPlace, opacityButtons, buttonLike, buttonDeleteCard };
+export { openPopupImage, closePopupImage, showPopupProfile, closePopupProfile, showPopupPlace, closePopupPlace, opacityButtons, buttonLike, buttonDeleteCard };
