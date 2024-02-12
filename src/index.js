d531a27055c082da51c5dc7d80eda01e9063b5e9
Jorge R. Sepulveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-02-12 19:06:02 -0300

    feat:Instalación y configuración de webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { data, config, sectionCard, btnPopupEdit, btnPopupPlace, profileName, profileAbout, inputProfileName, inputProfileAbout, inputNamePlace, inputUrlPlace, popupFormProfile, popupFormPlace } from './scripts/utils.js';
-import Card from './scripts/Card.js';
-import Section from './scripts/Section.js';
-import UserInfo from './scripts/UserInfo.js';
-import FormValidator from './scripts/FormValidator.js';
-import PopupWithImage from './scripts/PopupWithImage.js';
-import PopupWithForm from './scripts/PopupWithForm.js';
+import './pages/index.css';
+import { data, config, sectionCard, btnPopupEdit, btnPopupPlace, profileName, profileAbout, inputProfileName, inputProfileAbout, inputNamePlace, inputUrlPlace, popupFormProfile, popupFormPlace } from './components/utils.js';
+import Card from './components/Card.js';
+import Section from './components/Section.js';
+import UserInfo from './components/UserInfo.js';
+import FormValidator from './components/FormValidator.js';
+import PopupWithImage from './components/PopupWithImage.js';
+import PopupWithForm from './components/PopupWithForm.js';
 
 const formValidatorProfile = new FormValidator(config, popupFormProfile);
 formValidatorProfile.enableValidation();
