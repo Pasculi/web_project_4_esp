31840c43b43e3f1e46556c4696ac156f9326c19d
Jorge R. Sepulveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-04-01 11:27:35 -0300

    fix:Correcciones automáticas

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -7,7 +7,7 @@ export default class PopupWithConfirmation extends PopUp {
     this._handelConfirm = this._handelConfirm.bind(this);
     this._handleClickConfirm = this._handleClickConfirm.bind(this)
     this._callback = callback;
-    this._buttonDelete = document.querySelector('.popup__button--delete-confirm');
+    this._buttonDelete = document.querySelector('.popup__confirm-button-delete');
   }
   _handelConfirm(evt) {
     evt.preventDefault();
@@ -28,8 +28,8 @@ export default class PopupWithConfirmation extends PopUp {
   }
 
   setEventListeners() {
-    super.setEventListeners(); 
-    this.popup.querySelector('form').addEventListener('submit', this._handelConfirm)    
+    super.setEventListeners();
+    this.popup.querySelector('form').addEventListener('submit', this._handelConfirm)
     this._buttonDelete.addEventListener('click', this._handleClickConfirm)
   }
 }
